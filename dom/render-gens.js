e24a94c4b7b900bdbecae47820e90bcfe0f73afa
Jim Kang <jimkang@gmail.com>
2021-07-18 03:41:29 -0400

    Switch to graphical approach. Add enzymes that break apart words.

--- dom/render-gens.js
+++ dom/render-gens.js
@@ -1,4 +1,6 @@
 import { select } from 'd3-selection';
+import { zoom } from 'd3-zoom';
+import { drag } from 'd3-drag';
 import accessor from 'accessor';
 
 export function renderGens({ generations, onFerment }) {
@@ -7,29 +9,70 @@ export function renderGens({ generations, onFerment }) {
     .data(generations, accessor());
   allGensSel.exit().remove();
   var newGens = allGensSel.enter().append('li').classed('generation', true);
-  newGens.append('p').classed('substrates', true);
-  newGens.append('p').classed('enzymes', true);
+  var boards = newGens.append('svg').attr('viewBox', '0 0 100 50');
+  var zoomRoot = boards.append('g');
+  var zoomer = zoom().on('zoom', onZoom);
+  boards.call(zoomer);
+
+  zoomRoot.append('g').classed('substrates', true);
+  zoomRoot.append('g').classed('enzymes', true);
   newGens
     .append('button')
     .text('Ferment next generation')
     .on('click', onClickFerment);
+
   var gens = allGensSel.merge(newGens);
-  gens
+
+  var subSel = gens
     .select('.substrates')
     .selectAll('.substrate')
-    .data(accessor('substrates'))
-    .join('span')
+    .data(accessor('substrates'));
+  subSel.exit().remove();
+  var newSubSel = subSel
+    .enter()
+    .append('g')
     .classed('substrate', true)
-    .text(accessor('identity'));
-  gens
+    .call(drag().on('drag', onDrag));
+  //newSubSel.append('circle').attr('r', 5);
+  newSubSel.append('text').attr('dy', 0.5);
+  newSubSel
+    .merge(subSel)
+    .attr('transform', getTransform)
+    .select('text')
+    .text(accessor('word'));
+
+  var enzSel = gens
     .select('.enzymes')
     .selectAll('.enzyme')
-    .data(accessor('enzymes'))
-    .join('span')
+    .data(accessor('enzymes'));
+  enzSel.exit().remove();
+  var newEnzSel = enzSel
+    .enter()
+    .append('g')
     .classed('enzyme', true)
-    .text(accessor('identity'));
+    .call(drag().on('drag', onDrag));
+  newEnzSel.append('circle').attr('r', 10);
+  newEnzSel.append('text').attr('dy', 1);
+  newEnzSel
+    .merge(enzSel)
+    .attr('transform', getTransform)
+    .select('text')
+    .text(accessor('desc'));
 
-  function onClickFerment(generation) {
+  function onClickFerment(e, generation) {
     onFerment(generation);
   }
+
+  function onZoom(event) {
+    zoomRoot.attr('transform', event.transform);
+  }
+}
+
+function onDrag(event, d) {
+  d.pos = [event.x, event.y];
+  select(this).attr('transform', getTransform(d));
+}
+
+function getTransform(d) {
+  return `translate(${d.pos[0]}, ${d.pos[1]})`;
 }
--- updaters/ferment.js
+++ updaters/ferment.js
@@ -1,65 +1,112 @@
 import { queue } from 'd3-queue';
-import { range } from 'd3-array';
 import request from 'basic-browser-request';
 import bodyMover from 'request-body-mover';
 import cloneDeep from 'lodash.clonedeep';
+import math from 'basic-2d-math';
 
 const w2vBaseURL = 'http://localhost:9666/';
 
 var inertSubstrates = ['the', 'a', 'an', 'but', 'and'];
 
 export function ferment({ generation, probable }, done) {
-  var enzymes = probable.shuffle(generation.enzymes);
-  var targetSubstrateIndexes = probable
-    .shuffle(range(generation.substrates.length))
-    .slice(0, enzymes.length);
-  var q = queue(4);
-  const limit =
-    enzymes.length > targetSubstrateIndexes.length
-      ? enzymes.length
-      : targetSubstrateIndexes.length;
+  var substrates = cloneDeep(generation.substrates);
+  var nextGen = {
+    substrates,
+    enzymes: generation.enzymes,
+    id: +generation.id + 1,
+  };
+  var splicePacks = [];
 
-  for (let i = 0; i < limit; ++i) {
-    const substIndex = targetSubstrateIndexes[i];
-    q.defer(getSums, substIndex, generation.substrates[substIndex], enzymes[i]);
+  var q = queue();
+  generation.enzymes.forEach((enzyme) => q.defer(runEnzyme, enzyme));
+  q.awaitAll(passNextGen);
+
+  function passNextGen(error) {
+    splicePacks
+      .sort(compareFirstDesc)
+      .forEach((spliceArgs) => substrates.splice.apply(substrates, spliceArgs));
+    done(error, nextGen);
   }
 
-  q.awaitAll(updateGeneration);
+  function runEnzyme(enzyme, enzymeDone) {
+    var targetSubstrateIndexes = getIndexesOfSubstratesInRange(enzyme.pos);
+    var addQ = queue(4);
 
-  function updateGeneration(error, newSubstrateChoices) {
-    if (error) {
-      done(error);
-      return;
+    for (let i = 0; i < targetSubstrateIndexes.length; ++i) {
+      const substIndex = targetSubstrateIndexes[i];
+      addQ.defer(
+        getSums,
+        substIndex,
+        generation.substrates[substIndex].word,
+        enzyme.action === 'add' ? enzyme.supplement : null
+      );
     }
 
-    var substrates = cloneDeep(generation.substrates);
-    newSubstrateChoices.forEach(updateFromResults);
-    var nextGen = {
-      substrates,
-      enzymes,
-      id: +generation.id + 1,
-    };
-    done(null, nextGen);
+    addQ.awaitAll(updateGeneration);
 
-    function updateFromResults(pack) {
-      var result = probable.pick(pack.results);
-      if (result.word) {
-        substrates[pack.substIndex] = result.word;
+    function updateGeneration(error, newSubstrateChoices) {
+      if (error) {
+        enzymeDone(error);
+        return;
+      }
+
+      for (let j = newSubstrateChoices.length - 1; j > -1; --j) {
+        let pack = newSubstrateChoices[j];
+
+        if (enzyme.action === 'add') {
+          var result = probable.pick(pack.results);
+          if (result.word) {
+            substrates[pack.substIndex].word = result.word;
+          }
+        } else if (enzyme.action === 'break') {
+          let spliceArgs = [pack.substIndex, 1];
+          let results = probable.shuffle(pack.results).slice(0, 2);
+          if (results.length > 0 && results[0].word) {
+            let newSubA = cloneDeep(substrates[pack.substIndex]);
+            newSubA.pos[1] -= 3;
+            newSubA.word = results[0].word;
+            spliceArgs.push(newSubA);
+            if (results.length > 1 && results[1].word) {
+              let newSubB = cloneDeep(substrates[pack.substIndex]);
+              newSubB.pos[1] += 3;
+              newSubB.word = results[1].word;
+              spliceArgs.push(newSubB);
+            }
+            splicePacks.push(spliceArgs);
+          }
+        }
+      }
+
+      enzymeDone();
+    }
+  }
+
+  function getIndexesOfSubstratesInRange(pos) {
+    var indexes = [];
+    for (let i = 0; i < substrates.length; ++i) {
+      if (
+        math.getVectorMagnitude(math.subtractPairs(pos, substrates[i].pos)) <=
+        10
+      ) {
+        indexes.push(i);
       }
     }
+    return indexes;
   }
 }
 
-function getSums(index, substrate, enzyme, done) {
-  if (inertSubstrates.includes(substrate)) {
+function getSums(index, wordA, wordB, done) {
+  if (inertSubstrates.includes(wordA)) {
     setTimeout(done, 0, null, {
       substIndex: index,
-      results: [{ word: substrate, distance: 0 }],
+      results: [{ word: wordA, distance: 0 }],
     });
     return;
   }
 
-  const url = `${w2vBaseURL}/neighbors?words=${substrate},${enzyme}&quantity=10&operation=add`;
+  const url = `${w2vBaseURL}/neighbors?words=${wordA}${
+    wordB ? ',' + wordB : ''
+  }&quantity=10&operation=add`;
   request({ method: 'GET', url, json: true }, bodyMover(wrapResults));
 
   function wrapResults(error, results) {
@@ -69,4 +116,8 @@ function getSums(index, substrate, enzyme, done) {
     }
     done(null, { substIndex: index, results });
   }
+}
+
+function compareFirstDesc(a, b) {
+  return a[0] < b[0] ? 1 : -1;
 }
